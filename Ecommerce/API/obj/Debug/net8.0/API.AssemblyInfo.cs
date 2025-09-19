3b4baab65b54a913c0c39b6a87c2b3dd404cf080
gix8 <giovanixisarndt@gmail.com>
2025-09-19 10:53:02 -0300

    adicionadas pacotes para sqlite

--- Ecommerce/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Ecommerce/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87c9b823fc714cee5d5605ea00b9684beccec15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5261f5aefa11c4add90518b88bba750608cd66c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

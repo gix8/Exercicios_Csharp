544d13f5561790cca5a99ff85b9ac26d7a6d178b
gix8 <giovanixisarndt@gmail.com>
2025-09-19 11:10:06 -0300

    .

---
+++ Ecommerce/API/Models/AppDataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class AppDataContext : DbContext
+    {
+         public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=nomeDoSeuBanco.db");
+    }
+    }
+}

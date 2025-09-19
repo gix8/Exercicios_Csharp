3ee744c192a78bbee5dcf92014de2219fc824c73
gix8 <giovanixisarndt@gmail.com>
2025-09-19 09:35:48 -0300

    novas funções mapDelete e MapPut

--- Ecommerce/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Ecommerce/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ad6d224f9ab23d9c727fab95e077f4df91e69c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+812f6e1bb0a1c9e431f852a837da36a03b6cf3f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
